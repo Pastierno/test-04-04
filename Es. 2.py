3acb2919a7ae8c5074386e5f5db5fde540d504ea
Pastierno <marcopatierno.m@gmail.com>
2025-04-04 10:59:52 +0200

    Es. 1 completato

---
+++ Es. 2.py
@@ -0,0 +1,3 @@
+# Andare a creare un sistema ripetibile che si occupa di gestire la lunghezza delle stringhe e salvarle, 
+# l'utente potrà continuare a inserire dati finché la stringa inserita e più lunga della precedente, 
+# alla fine stamperà l'insieme delle stringhe date in input divise da virgole e quante stringhe ha inserito.
--- Es.1.py
+++ Es.1.py
@@ -1,8 +1,39 @@
-num = [1,2,'3']
-sum = 0
-for n in num:
-    if n% 2 == 0:
-        sum += n
+# Crea un sistema ripetibile che si occupi di dividere su tre possibili liste i tipi basilari di 
+# dato che riceve in entrata. Deve poter stampare una lista singola o tutte.  Si deve ripetere X 
+# volte definite all'inizio dall'utente
+
+
+
+# Liste vuote che conterrano i tipi di dato
+num = []
+stri = []
+boolean = []
+
+# Controllore per ciclo
+control = True
+while control:
+    user_input = input('Inserisci un intero, una stringa o un booleano: (€ per uscire, = per stampare le liste)\n')
+    
+    # Condizioni:
+    if user_input == '€': # Esce
+        print('Ciao!')
+        break
+    elif user_input == '=': # Stampa i valori
+        print(num)
+        print(stri)
+        print(boolean)
+    elif user_input.isdigit():
+        num.append(int(user_input))
+        print(f'{user_input} aggiunto alla lista')
+        continue
+    elif user_input == 'True' or user_input == 'False':
+        boolean.append(bool(user_input))
+        print(f'{user_input} aggiunto alla lista')
+        continue
     else:
-        sum -= n
-print(sum)
+        stri.append(user_input)
+        print(f'{user_input} aggiunto alla lista')
+
+        
+    
+    
